11aab9f79346ff2a452636ba273e1fa8ed35703d
KariukiAbel <abelmuriithi@gmail.com>
2019-07-02 15:25:32 +0300

    Simlpe

--- addpurchase.php
+++ addpurchase.php
@@ -18,19 +18,21 @@
 <?php include 'links.php';?>
 <div class="container-fluid">
 <div class="row">
-    <div class="col-md-2"></div>
-        <div class="col-md-8">
-            <div style="border-style: groove;padding: 15px;">
+    <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <div>
+            <div class="row m-sm-auto">
+           <div class="col-sm-5" style="border-style: groove;padding: 15px;border-radius: 5px;">
         <h2 style="text-align: center;">Supplier Details</h2>
         <form action="" method="post" enctype="multipart/form-data">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="email">Name of Supplier:</label>
                         <input type="text" class="form-control" name="name">
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="pwd">Address:</label>
                         <input type="text" class="form-control" name="address">
@@ -38,13 +40,13 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="pwd">Telephone Number:</label>
                         <input type="text" class="form-control" name="phone">
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="pwd">Email:</label>
                         <input type="email" class="form-control" name="mail">
@@ -54,13 +56,13 @@
             </div>
 
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="pwd">Location:</label>
                         <input type="text" class="form-control" name="loc">
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-sm-6">
                     <div class="form-group">
                         <label for="pwd">KRA PIN:</label>
                         <input type="text" class="form-control" name="pin">
@@ -69,33 +71,27 @@
 
             </div>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-sm-6">
 
                     <div class="form-group">
                         <label for="pwd">Unit Price:</label>
                         <input type="number" class="form-control" name="price">
                     </div>
                 </div>
-                <div class="col-md-6"></div>
 
             </div>
-            <div class="row">
-                <div class="col-md-6"></div>
-                <div class="col-md-6"></div>
-
-            </div>
-            </div>
-
-            <div style="border-style: groove;padding: 15px;">
-           <h2 style="text-align: center;">Product Details</h2>
+           </div>
+                <div class="col-sm-1"></div>
+            <div class="col-sm-5" style="border-style: groove;padding: 15px; border-radius: 5px;">
+                   <h2 style="text-align: center;">Product Details</h2>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-sm-6">
                         <div class="form-group">
                             <label for="email">Date and Time of purchase:</label>
                             <input type="datetime-local" class="form-control" name="datetime">
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-sm-6">
                         <div class="form-group">
                             <label for="pwd">Product:</label>
                             <input type="text" class="form-control" name="product">
@@ -104,13 +100,13 @@
 
                 </div>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-sm-6">
                         <div class="form-group">
                             <label for="pwd">Description:</label>
                             <input type="text" class="form-control" name="description">
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-sm-6">
                         <div class="form-group">
                             <label for="pwd">Quantity:</label>
                             <input type="number" class="form-control" name="quantity">
@@ -119,23 +115,30 @@
 
                 </div>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-sm-6">
                         <div class="form-group">
                             <label for="pwd">Total Price:</label>
                             <input type="number" class="form-control" name="total">
                         </div>
                     </div>
-                    <div class="col-md-6"></div>
+                    <div class="col-sm-6"></div>
 
                 </div>
+            </div>
+</div>
+            <div class="row">
+                <div class="col-sm-4 offset-4" style="padding: 2%">
+                    <button type="submit" class="btn btn-danger btn-block">Submit</button>
+                    <br><br>
+                </div>
+            </div>
 
-                <button type="submit" class="btn btn-danger btn-block">Submit</button>
-                <br><br>
             </form>
-    </div>
-
+               </div>
         </div>
-    <div class="col-md-2"></div>
+<div class="col-sm-1"></div>
+
+</div>
 </div>
 </body>
 </html>
